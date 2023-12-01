7a1442c65ea2a4a030237d4030e00a6573c1198b
DOGERAZ <123812634+DOGERAZ@users.noreply.github.com>
2023-12-01 18:52:53 +0300

    urls.py

--- cadastre/urls.py
+++ cadastre/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import query, result, ping, history
 
 urlpatterns = [
-    path('query/', query, name='query'), # type: ignore
+    path('query/', query, name='query'), 
     path('result/', result, name='result'),
     path('ping/', ping, name='ping'),
     path('history/', history, name='history'),
