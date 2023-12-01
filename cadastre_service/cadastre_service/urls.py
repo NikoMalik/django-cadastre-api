98ab0acc84d1b6d1da1b65ef96f1177f4cf0de91
DOGERAZ <123812634+DOGERAZ@users.noreply.github.com>
2023-12-01 18:51:16 +0300

    urls.py

--- cadastre_service/cadastre_service/urls.py
+++ cadastre_service/cadastre_service/urls.py
@@ -1,8 +1,7 @@
-# cadastre_service/urls.py
+
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cadastre/', include('cadastre.urls')),  # Импортируем URL-паттерны из приложения cadastre
-]
+    path('cadastre/', include('cadastre.urls')), 
